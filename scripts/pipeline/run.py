303205c607006f444c04db312f5d4ff3de5ba6b9
fkubota <rkkubotay@gmail.com>
2019-12-20 09:13:19 +0900

    update

--- scripts/pipeline/run.py
+++ scripts/pipeline/run.py
@@ -50,12 +50,17 @@ def preparation_logger(log_name):
 
 def run(model, n_fold, feat_grps, model_params):
     rh = ResultHandler(model.__name__)
+    print('==============', rh.name, '=======================================')
     logger, sh = preparation_logger(rh.name)
+    logger.info('********************************************************')
     logger.info('******************** start pipeline ********************')
+    logger.info('********************************************************')
     runner = Runner(model, n_fold, feat_grps, model_params, rh)
     runner.run_train_cv()
     runner.run_predict_cv()
-    logger.info('******************** end pipeline ********************')
+    logger.info('********************************************************')
+    logger.info('********************* end pipeline *********************')
+    logger.info('********************************************************')
 
 
 if __name__ == '__main__':
@@ -72,24 +77,28 @@ if __name__ == '__main__':
         'objective': 'binary:logistic',
         'eval_metric': 'logloss',
         'num_class': 1,
-        'max_depth': 12,
-        'eta': 0.1,
+        'max_depth': [100, 200, 300],
+        'eta': 0.01,
         'min_child_weight': 10,
         'subsample': 0.9,
         'colsample_bytree': 0.8,
-        'silent': 1,
+        # 'silent': 1,
         'random_state': 71,
-        'num_round': 10,
-        'early_stopping_rounds': 10,
+        'verbosity': 2,
+        'verbose': 10,
+        'num_round': 1000,
+        'early_stopping_rounds': 100,
     }
 
     # ==============================================================
 
-    # xgboostによる学習・予測
+    # parse_params
+    dict_params = params_xgb
     model = ModelXGB
-    run(model=ModelXGB,
-        n_fold=n_fold,
-        feat_grps=feat_grps,
-        model_params=params_xgb
-        )
-# Submission.create_submission('xgb1')
+    dict_params_list = Util.parse_dict_param(dict_params)
+    for params in dict_params_list:
+        run(model=model,
+            n_fold=n_fold,
+            feat_grps=feat_grps,
+            model_params=params)
+    # Submission.create_submission('xgb1')
